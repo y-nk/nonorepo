1c19fdbbff0308d1a248da5610ffe440d6cdf6ce
Ivan Voskoboinyk <ivan@voskoboinyk.com>
2023-10-31 14:54:09 +0200

    Fix Next.js compatibility: "global is not defined"

--- cached-fetch/src/createFetch.ts
+++ cached-fetch/src/createFetch.ts
@@ -6,5 +6,5 @@ import { createDedupedFetch } from "./createDedupedFetch";
 let _fetch: Fetch
 
 export function createFetch() {
-  return  _fetch ??= createDedupedFetch(createCachedFetch(global.fetch))
+  return  _fetch ??= createDedupedFetch(createCachedFetch(fetch))
 }
