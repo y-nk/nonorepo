3612ca5d7518e68e4e0a1f00fc3864f694605c3d
Julien Barbay <julien.barbay@gmail.com>
2023-12-01 12:17:04 +0700

    fix(lucide-svg): correct pascal to kebab conversion

--- lucide-svg/build.mjs
+++ lucide-svg/build.mjs
@@ -10,6 +10,8 @@ const {
   ...configs
 } = lucide
 
+console.log({ icons })
+
 const { window } = new JSDOM(``);
 globalThis.document = window.document
 
@@ -21,7 +23,11 @@ rmSync('./dist', {
 mkdirSync('./dist')
 
 for (const [key, val] of Object.entries(configs)) {
-  const fileName = `${kebab(key).slice(1)}.svg`
+  const iconName = kebab(key)
+    .slice(1)
+    .replace(/([a-z])([0-9])/g, '$1-$2')
+
+  const fileName = `${iconName}.svg`
   const fileContent = createElement(val).outerHTML
 
   writeFileSync(`./dist/${fileName}`, fileContent, 'utf-8')
